164a381ec21fa8ba843c05e04bf524a58192c29a
jedrzejmatuszak <jedrzej.matuszak@gmail.com>
2019-06-14 07:37:14 +0200

    chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
+login = LoginManager(app)
+login.login_view = 'login'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
