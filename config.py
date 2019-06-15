9f1a1ce6a48dfac1700d785e92924f0c8bb0d9ff
jedrzejmatuszak <jedrzej.matuszak@gmail.com>
2019-06-15 19:00:03 +0200

    chapter 9

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['blady.dev@zohomail.eu']
+    POSTS_PER_PAGE = 25
