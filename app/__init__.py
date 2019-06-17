6c7a008a3dbe3d6eed1665de8dffd39b0ae2180d
jedrzejmatuszak <jedrzej.matuszak@gmail.com>
2019-06-17 21:05:13 +0200

    chapter 13

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -9,30 +9,22 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask_babel import lazy_gettext as _l
 
 app = Flask(__name__)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
-    # if not os.path.exists('logs'):
-    #     os.mkdir('logs')
-    # file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-    #                                    backupCount=10)
-    # file_handler.setFormatter(logging.Formatter(
-    #     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    # ))
-    # file_handler.setLevel(logging.INFO)
-    # app.logger.addHandler(file_handler)
-    # app.logger.setLevel(logging.INFO)
-    # app.logger.info('Microblog startup')
-
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -44,9 +36,25 @@ if not app.debug:
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-            credentials=auth, secure=secure
-        )
+            credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -14,3 +14,4 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['bladybladowicz@gmail.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'pl', 'es']
